587a1389efe8550f021a1c92562c16a4bd386eff
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-04-04 00:33:19 +0500

    day first

---
+++ 01_basics/basics.py
@@ -0,0 +1,24 @@
+print('Hello world')
+
+# operation order PEMDAS
+#(Parenthesis,Exponents,Multiplication,Division,Addition,Subtraction)
+# ** does not support the left-to-right associativity
+exp1 = 6*8/2**3-2*2
+print(exp1)
+
+exp2 = 4**3**2
+print(exp2)
+
+exp3 = (4**3)**2
+print(exp3)
+
+print(4**5.0)
+
+print(7/8) #Division
+
+print(7//8) # Integer Division
+
+# Modulo
+print(100%2)
+
+print(100 % 3)
