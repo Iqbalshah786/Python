1533b19f8acb99272aa3f9da8848731496b6c346
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-04-04 14:21:54 +0500

    day 2

---
+++ 02_Data_types/casting.py
@@ -0,0 +1,32 @@
+# Integer Casting (int()):
+x = int(3.14)
+print(x)  # Output: 3
+
+# Float Casting (float()):
+y = float("3.14")
+print(y)  # Output: 3.14
+
+# String Casting (str()):
+z = str(123)
+print(z)  # Output: "123"
+
+# List Casting (list()): 
+t = (1,2,3)
+lst = list(t)
+print(lst) # # Output: [1, 2, 3]
+
+# Tuple Casting (tuple()):
+lst = [1,2,3]
+t = tuple(lst)
+print(t) # Output: (1, 2, 3)
+
+# Set Casting (set()):
+lst = [1,2,3,3,2,1]
+s = set(lst)
+print(s) # Output: {1, 2, 3}
+
+# Dictionary Casting (dict()):
+lst = [("a",1),("b",2),("c",3)]
+d = dict(lst)
+print(d) # Output: {'a': 1, 'b': 2, 'c': 3}
+
