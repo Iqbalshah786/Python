ee77fd76b7c466738aea0b5ee8171004a7a21be5
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-04-04 12:56:00 +0500

    day second

---
+++ 02_Data_types/variable_naming.py
@@ -0,0 +1,10 @@
+y = 100
+print(y+1)
+
+number_of_dogs = 3 
+print(number_of_dogs * 4)
+print(number_of_dogs)
+
+# you can also assign values to more than one variable at once
+first , second , third = 1,2,3
+print(first,second,third)
