6a0e2218224481767aa2125b62fbf93c205e8d09
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-04-05 11:35:56 +0500

    day 3

---
+++ 03_Numbers/numbers.py
@@ -0,0 +1,24 @@
+import math , random
+
+print(math.floor(3.4)) # 3
+print(math.floor(-3.4)) # -4
+print(math.trunc(2.8)) # trunc towards zero
+print(math.trunc(-2.8)) 
+
+
+print(999999999999999999999999999 + 1)
+print(999999999999999999999999999 * 2.4)
+print(2 ** 200)
+# Imagenary numbers
+print(2 + 2j)
+print((2 + 2j) * 3)  # 6 + 6j
+print(random.random())
+print(random.randint(1,100))
+l1 = [1,2,3,4,5,6]
+print(random.choice(l1))
+random.shuffle(l1)
+print(l1)
+print(True == 1)
+print(False == 0)
+print(True is 1)
+print(True + 5) # 6
